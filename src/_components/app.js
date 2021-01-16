f783daab7596f6df9f5b0d5cf60457b6da4aaea5
FellipeOliveiraBartalini <fellipeoliveiranobre@hotmail.com>
2021-01-16 10:11:38 -0300

    teste

--- src/_components/app.js
+++ src/_components/app.js
@@ -8,6 +8,7 @@ function App() {
         <div id="main-div">
             <h1>Simulador de Carro Manual!</h1>
             <span id="tutorial">
+                <h1>Teste</h1>
                 <p>Como jogar:</p>
                 <p>Você só trocará de marcha quando a Embreiagem estiver pisada.</p>
                 <p>Controles: <br/>A = Embreiagem <br/>S = Freio <br/> D = Acelerador <br/> Shift = Subir marcha <br/> Ctrl = Descer marcha</p>
